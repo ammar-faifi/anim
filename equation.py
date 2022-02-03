138c81840cacc670a0f06d3a7920611a9f3ed3c3
ammar-faifi <a@ammarf.com>
2022-02-03 22:35:50 +0300

    add tips use new way fot potential graph

--- equation.py
+++ equation.py
@@ -5,12 +5,11 @@ class Eq(Scene):
     def construct(self):
         title = Tex('An $e^-$ inside potential').to_corner(UP + LEFT)
         axes = Axes(
-            x_range=[-10, 10, 11],
+            x_range=[-10, 10, 4],
             y_range=[-1.5, 1.5, 4],
             x_length=6,
             y_length=3,
             axis_config={"color": WHITE},
-            tips=False,
         ).shift(2*DOWN)
         plate_l = Rectangle(color=RED, height=6, width=2,
                             fill_opacity=1).shift(4*LEFT)
@@ -36,18 +35,21 @@ class Eq(Scene):
         self.play(Create(el))
         self.play(set_up.animate.scale(0.5).shift(1.2 * UP))
 
-        disc = [axes.point_to_coords(plate_l.get_right())[0],
-                axes.point_to_coords(plate_r.get_left())[0]]
+        disc = [axes.point_to_coords(plate_r.get_left())[0]]
 
         def potential(x):
-            if disc[0] <= x <= disc[1]:
-                return 0
-            else:
+            if x >= disc[0]:
                 return 1
+            else:
+                return 0
 
-        plot = axes.plot(potential, discontinuities=disc, dt=0.1, color=WHITE)
+        plot = axes.plot(potential, discontinuities=disc, dt=0.1, color=BLUE)
         plot.make_jagged()
+        eps = 1e-8
+        plot2 = axes.plot_line_graph(
+            [-8, disc[0], disc[0]+eps, disc[0]+2], [0, 0, 1, 1], add_vertex_dots=False)
 
+        self.wait(2)
         self.play(Create(axes))
-        self.play(FadeIn(plot))
+        self.play(FadeIn(plot2))
         self.wait(1)
